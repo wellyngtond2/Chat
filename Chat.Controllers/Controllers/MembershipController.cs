75d880e04901a07d0b240dd662290848dc87c065
Wellyngton Borges <wellyngton.borges@devscope.net>
2022-11-23 23:06:59 -0300

    added membership controller

---
+++ Chat.Controllers/Controllers/MembershipController.cs
@@ -0,0 +1,25 @@
+﻿using Chat.DataContracts.Membership.Request;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembershipController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MembershipController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterMembershipRequest request)
+        {
+            await _mediator.Send(request);
+            return Ok();
+        }
+    }
+}
